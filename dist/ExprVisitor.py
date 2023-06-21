74ceabd7cd574b9264dde6347cbdd1626fa42bf4
Glonk223 <janek.stop@gmail.com>
2023-06-21 14:35:11 +0200

    Fixed constructor and access bugs

--- dist/ExprVisitor.py
+++ dist/ExprVisitor.py
@@ -254,16 +254,12 @@ class ExprVisitor(ParseTreeVisitor):
             new_parameters = [[],[]]
 
         constructor_dict = {}
-        print(new_parameters)
-        for x in new_parameters[1]:
-            constructor_dict[x] = x;
+
         for v in variables:
             splited = ''
             if '=' in v.getText():
                 splited = v.getText().split('=')
             if splited != '' and splited[1] in new_parameters[1]:
-                if splited[1] in constructor_dict.keys():
-                    constructor_dict.pop(splited[1])
                 name2 = v.IDENTIFIER().getText()
                 constructor_dict[name2] = splited[1]
 
